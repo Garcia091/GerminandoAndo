0834c679fa25b440faa1ef8d8b0f1508ab96016c
Silvia García Sánchez <30872921+Garcia091@users.noreply.github.com>
2023-10-26 21:51:15 -0500

    germinandoando

---
+++ js/data.js
@@ -0,0 +1,54 @@
+export const Gusto = [
+    {
+        id:"01",
+        nombre:"ACEITE DE AJONJOLÍ",
+        descripcion:"ACEITE DE AJONJOLÍ<br><br><br> Aceite ajonjolí 250 ml <br> prensado en frio| 17.000 <br><br> Aceite ajonjolí 500 ml <br> prensado en frio |30.000",
+        imagen:"https://res.cloudinary.com/db9wh5uvt/image/upload/v1698357161/Imagen10_csnp6d.jpg"
+    },
+    {
+        id:"02",
+        nombre:"Gatos",
+        descripcion:"Me gusta las",
+        imagen:"https://okdiario.com/img/2023/04/10/-es-malo-que-los-gatos-coman-moscas-y-mosquitos-635x358.jpg"
+    },
+    {
+        id:"03",
+        nombre:"Conejos",
+        descripcion:"Me gusta ...",
+        imagen:"https://t2.ea.ltmcdn.com/es/posts/6/8/0/razas_de_conejos_enanos_o_toy_24086_600_square.jpg"  
+    },
+    {
+        id:"01",
+        nombre:"Aceite de ajonjolí",
+        descripcion:"Me gusta las",
+        imagen:"https://okdiario.com/img/2023/04/10/-es-malo-que-los-gatos-coman-moscas-y-mosquitos-635x358.jpg"
+    },
+    {
+        id:"02",
+        nombre:"Gatos",
+        descripcion:"Me gusta las",
+        imagen:"https://okdiario.com/img/2023/04/10/-es-malo-que-los-gatos-coman-moscas-y-mosquitos-635x358.jpg"
+    },
+    {
+        id:"03",
+        nombre:"Conejos",
+        descripcion:"Me gusta ...",
+        imagen:"https://t2.ea.ltmcdn.com/es/posts/6/8/0/razas_de_conejos_enanos_o_toy_24086_600_square.jpg"  
+    }
+]
+
+export const TALLER = [
+    {
+        id:"01",
+        imagen:"https://res.cloudinary.com/db9wh5uvt/image/upload/v1698370661/Imagen7_e11sqj.jpg"
+    },
+    {
+        id:"02",
+        imagen:"https://res.cloudinary.com/db9wh5uvt/image/upload/v1698370653/Imagen11_jr9tfj.jpg"
+    },
+    {
+        id:"03",
+        imagen:"https://res.cloudinary.com/db9wh5uvt/image/upload/v1698370653/Imagen10_cwrs2o.jpg"
+    }
+
+]
